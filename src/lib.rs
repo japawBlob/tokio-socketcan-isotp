3a3fce15cd9486f6e965eaf693a108b7fc45f19f
Jakub Jíra <jirajak2@fel.cvut.cz>
2023-01-20 23:26:54 +0100

    repository cleanded to bare minimumn. Reading and writting done by Futures.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-
 use socketcan_isotp::{self, RECV_BUFFER_SIZE};
 pub use socketcan_isotp::{Id, StandardId, Error};
 use std::io;
 use std::os::raw::c_int;
-use std::os::unix::io::{AsRawFd, FromRawFd, RawFd}; // maybe use RawFd from unix::prelude
-// use std::{future::Future};
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use futures::prelude::*;
@@ -12,22 +10,45 @@ use futures::ready;
 use mio::{event, unix::SourceFd, Interest, Registry, Token};
 use tokio::io::unix::AsyncFd;
 
-pub struct IsoTpWriteFuture {
-    socket: IsoTpSocket,
+/// Future for writing data to IsoTPSocket
+pub struct IsoTpWriteFuture<'a> {
+    socket: &'a IsoTpSocket,
     packet: Vec<u8>,
 }
 
-impl Future for IsoTpWriteFuture {
+impl Future for IsoTpWriteFuture<'_> {
     type Output = io::Result<()>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // eprintln!("polling i guess");
-        let _ = ready!(self.socket.0.poll_write_ready(cx))?;
-        match self.socket.0.get_ref().0.write(&self.packet) {
-            Ok(_) => Poll::Ready(Ok(())),
-            Err(err) => Poll::Ready(Err(err)),
+        loop {
+            let _ = ready!(self.socket.0.poll_write_ready(cx))?;
+            match self.socket.0.get_ref().write_vec(&self.packet) {
+                Err(err) if err.kind() == io::ErrorKind::WouldBlock => continue,
+                Ok(_) => return Poll::Ready(Ok(())),
+                Err(err) => return Poll::Ready(Err(err))
+            }
         }
     }
+}
+
+/// Future for readint data from IsoTPSocket
+pub struct IsoTpReadFuture<'a> {
+    socket: &'a IsoTpSocket,
+}
+
+impl Future for IsoTpReadFuture<'_> {
+    type Output = io::Result<Vec<u8>>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        loop {
+            let mut ready_guard = ready!(self.socket.0.poll_read_ready(cx))?;
+            match ready_guard.try_io(|inner| inner.get_ref().read_to_vec()) {
+                Ok(result) => return Poll::Ready(result),
+                Err(_would_block) => continue,
+            }
+        }
+    }
+
 }
 
 pub struct EventedIsoTpSocket(socketcan_isotp::IsoTpSocket);
@@ -35,6 +56,9 @@ pub struct EventedIsoTpSocket(socketcan_isotp::IsoTpSocket);
 impl EventedIsoTpSocket {
     fn get_ref(&self) -> &socketcan_isotp::IsoTpSocket {
         &self.0
+    }
+    fn get_mut(&mut self) -> &mut socketcan_isotp::IsoTpSocket {
+        &mut self.0
     }
 }
 
@@ -44,6 +68,7 @@ impl AsRawFd for EventedIsoTpSocket {
     }
 }
 
+/// trait used for regstering the EventedIsoTpSocket to mio events
 impl event::Source for EventedIsoTpSocket {
     fn register(
         &mut self,
@@ -69,83 +94,36 @@ impl event::Source for EventedIsoTpSocket {
 }
 
 /// An asynchronous I/O wrapped socketcan_isotp::IsoTpSocket
-pub struct IsoTpSocket(AsyncFd<EventedIsoTpSocket>);
+pub struct IsoTpSocket(
+    AsyncFd<socketcan_isotp::IsoTpSocket>
+);
 
 impl IsoTpSocket {
     /// Open a named CAN device such as "vcan0"
     pub fn open(ifname: &str, src: impl Into<Id>, dst: impl Into<Id>) -> Result<IsoTpSocket, socketcan_isotp::Error> {
         let sock = socketcan_isotp::IsoTpSocket::open(ifname, src, dst)?;
         sock.set_nonblocking(true)?;
-        Ok(IsoTpSocket(AsyncFd::new(EventedIsoTpSocket(sock))?))
+        Ok(IsoTpSocket(AsyncFd::new(sock)?))
     }
 
     /// Open by kernel interface number
     pub fn open_if(if_index: c_int, src: impl Into<Id>, dst: impl Into<Id>) -> Result<IsoTpSocket, socketcan_isotp::Error> {
         let sock = socketcan_isotp::IsoTpSocket::open_if(if_index, src, dst)?;
         sock.set_nonblocking(true)?;
-        Ok(IsoTpSocket(AsyncFd::new(EventedIsoTpSocket(sock))?))
+        Ok(IsoTpSocket(AsyncFd::new(sock)?))
     }
 
     pub fn write_packet(&self, packet : Vec<u8>) -> Result<IsoTpWriteFuture, Error>{
         Ok(IsoTpWriteFuture {
-            socket: self.try_clone()?,
+            socket: &self,
             packet,
         })
     }
 
-    fn try_clone(&self) -> Result<Self, io::Error> {
-        let fd = self.0.get_ref().0.as_raw_fd();
-        /// eprintln!("whelp cloning");
-        unsafe {
-            // essentially we're cheating and making it cheaper/easier
-            // to manage multiple references to the socket by relying
-            // on the posix behaviour of `dup()` which essentially lets
-            // the kernel worry about keeping track of references;
-            // as long as one of the duplicated file descriptors is open
-            // the socket as a whole isn't going to be closed.
-            let new_fd = libc::dup(fd);
-            let new = socketcan_isotp::IsoTpSocket::from_raw_fd(new_fd);
-            Ok(IsoTpSocket(AsyncFd::new(EventedIsoTpSocket(new))?))
-        }
-    }
-}
-
-impl Stream for IsoTpSocket {
-    type Item = io::Result<Vec<u8>>;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        loop {
-            let mut ready_guard = ready!(self.0.poll_read_ready(cx))?;
-            match ready_guard.try_io(|inner| inner.get_ref().get_ref().read_to_buffer()) {
-                Ok(result) => return Poll::Ready(Some(result)),
-                Err(_would_block) => continue,
-            }
-        }
-    }
-}
-
-
-impl Sink<Vec<u8>> for IsoTpSocket {
-    type Error = io::Error;
-
-    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        let _ = ready!(self.0.poll_write_ready(cx))?;
-        Poll::Ready(Ok(()))
-    }
-
-    fn start_send(self: Pin<&mut Self>, item: Vec<u8>) -> Result<(), Self::Error> {
-        self.0.get_ref().0.write(&item)?;
-        Ok(())
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        let mut ready_guard = ready!(self.0.poll_write_ready(cx))?;
-        ready_guard.clear_ready();
-        Poll::Ready(Ok(()))
+    pub fn read_packet(&self) -> Result<IsoTpReadFuture, Error>{
+        Ok(IsoTpReadFuture {
+            socket: &self,
+        })
     }
 }
 
